63ed28f356bcdc4c9bb4f32c55e409d201b0a16b
Dov Friedman <dov@dovfriedman.com>
2014-08-04 21:21:48 -0400

    organization

--- get.php
+++ get.php
@@ -3,9 +3,9 @@
 include 'functions.php';
 include '/connections/idiorm_connection_personal.php';
 
-$usernames = ORM::for_table('usernames')->find_array();
-
 $decrypted_records = array();
+
+$usernames = ORM::for_table('usernames')->find_array();
 
 foreach($usernames as $username){
 	$username['decrypted_username'] = decrypt($username['username']);
